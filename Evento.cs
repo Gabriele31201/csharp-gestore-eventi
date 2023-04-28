02b0e6e3196285d84748e883d4ab603c8912ce0a
Gabriele31201 <gabri31201@gmail.com>
2023-04-28 09:59:01 +0200

    add constructor

--- Evento.cs
+++ Evento.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace GestoreEventi
 {
-    internal class Evento
+    public class Evento
     {
+        //attributes
+        private string Title;
+        private string Date;
+        private int MaxCapientEvent;
+        private int NumberOfSeatBooked;
+
+        //constructor
+        public Evento(string Title, string Date, int MaxCapientEvent, int NumberOfSeatBooked) 
+        {
+            this.Title = Title;
+            this.Date = Date;
+            this.MaxCapientEvent = MaxCapientEvent;
+            this.NumberOfSeatBooked = NumberOfSeatBooked;
+        }
     }
 }
